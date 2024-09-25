d1d9102d8da81b47696c9d8514f8e4fed87738f5
GrupoPIE04 <145173684+GrupoPIE04@users.noreply.github.com>
2024-09-25 20:16:29 -0300

    Quick sort fix de um erro de causa desconhecido
    reparo do quick sort após o mesmo dar um problema durante a coleta de dados de 360000 elementos

--- QuickSort.java
+++ QuickSort.java
@@ -28,57 +28,78 @@ public class QuickSort {
         System.out.println("Running time: " + (endTime - startTime) + " nanoseconds");
     }
 
-    private static int[] generateArray(int n, String order, boolean allowRepeats) {
-        Random random = new Random();
-        int[] array = new int[n];
+    private static int medianOfThree(int[] array, int low, int mid, int high) {
+        if (array[low] > array[mid]) {
+            swap(array, low, mid);
+        }
+        if (array[low] > array[high]) {
+            swap(array, low, high);
+        }
+        if (array[mid] > array[high]) {
+            swap(array, mid, high);
+        }
+        swap(array, mid, high - 1);
+        return array[high - 1];
+    }
 
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+private static int[] generateArray(int n, String order, boolean allowRepeats) {
+    Random random = new Random();
+    int[] array = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        array[i] = allowRepeats ? random.nextInt(n) : i;
+    }
+
+    if (order.equalsIgnoreCase("ascending")) {
+        Arrays.sort(array);
+    } else if (order.equalsIgnoreCase("descending")) {
+        Arrays.sort(array);
+        for (int i = 0; i < n / 2; i++) {
+            int temp = array[i];
+            array[i] = array[n - i - 1];
+            array[n - i - 1] = temp;
+        }
+    } else if (order.equalsIgnoreCase("random")) {
         for (int i = 0; i < n; i++) {
-            array[i] = allowRepeats ? random.nextInt(n) : i;
-        }
-
-        if (order.equalsIgnoreCase("ascending")) {
-            Arrays.sort(array);
-        } else if (order.equalsIgnoreCase("descending")) {
-            Arrays.sort(array);
-            for (int i = 0; i < n / 2; i++) {
-                int temp = array[i];
-                array[i] = array[n - i - 1];
-                array[n - i - 1] = temp;
-            }
-        } else if (order.equalsIgnoreCase("random") && !allowRepeats) {
-            for (int i = 0; i < n; i++) {
-                int randomIndex = random.nextInt(n);
-                int temp = array[randomIndex];
-                array[randomIndex] = array[i];
-                array[i] = temp;
-            }
-        }
-
-        return array;
-    }
-
-    private static void quickSort(int[] array, int low, int high) {
-        if (low < high) {
-            int pi = partition(array, low, high);
-            quickSort(array, low, pi - 1);
-            quickSort(array, pi + 1, high);
+            int randomIndex = random.nextInt(n);
+            int temp = array[randomIndex];
+            array[randomIndex] = array[i];
+            array[i] = temp;
         }
     }
 
-    private static int partition(int[] array, int low, int high) {
-        int pivot = array[high];
-        int i = (low - 1);
-        for (int j = low; j < high; j++) {
-            if (array[j] <= pivot) {
-                i++;
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
-        }
-        int temp = array[i + 1];
-        array[i + 1] = array[high];
-        array[high] = temp;
-        return i + 1;
+    return array;
+}
+
+private static void quickSort(int[] array, int low, int high) {
+    if (low < high) {
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
     }
 }
+
+private static int partition(int[] array, int low, int high) {
+    int mid = low + (high - low) / 2;
+    int pivot = medianOfThree(array, low, mid, high);
+    int i = low;
+    int j = high - 1;
+
+    while (true) {
+        while (i < high - 1 && array[++i] < pivot) {}
+        while (j > low && array[--j] > pivot) {}
+        if (i >= j) {
+            break;
+        }
+        swap(array, i, j);
+    }
+    swap(array, i, high - 1);
+    return i;
+}
+}
+
